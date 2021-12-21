8aeb2873e2d10056a9e3858380d18d5e25f2bb93
Rishon Jonathan R <66567635+RIshonRJ@users.noreply.github.com>
2021-12-21 16:10:17 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module juit.video.video {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+}
